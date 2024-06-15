7d5c0b741b165683c3d9e9449d234f43047c298f
DharagaiRavi <dharagai90ravi@gmail.com>
2024-06-15 10:32:13 +0530

    made some changes

---
+++ dataType.py
@@ -0,0 +1,83 @@
+#python data types
+#1.string Datatype-raw test
+'this is a string' "this is a string"
+#Multiple line quotation
+'''
+hello.i am 
+also a
+string
+'''
+"""
+Hello
+hi
+hellooo
+"""
+#2.Integer Datatype-23
+#3.Float Datatype-23.5
+#4.Complex Datatype-2j+3
+#5.range Datatype- range(0,100)
+#6.Boolean Datatype=True or False
+#---------------------------------------------
+#7.list Datatype-["apple","orange","grapes"]
+#8.tuple Datatype-("apple","orange","grapes")
+#9.set Datatype-{"apple","orange","grapes"}
+#10.dictionary Datatype={"fruit1":"apple","fruit2":"orange"}
+
+
+#Operators
+#+ - * /
+#% modulus
+#//floor division
+
+#-
+# --------------------------------------
+#print(5%2)
+#print(5/2)
+#print(5//2)  (if the output is in points,it just shows the number before the point,it wont round the value)
+
+#---------------------------------------
+#=,==,<,>,<=,>=
+
+#print(10==10)
+#print(10==0)
+# print(10>9)
+# print(10<9)
+# print(10>=9)
+# print(10<=9)
+
+#--------------------------------------------
+#+=, -= ,*= ,/=
+
+# x=5
+# x+=1
+# print(x)
+
+# x=5
+# x-=5
+# print(x)
+
+# x=5
+# x*=2
+# print(x)
+
+# x=5
+# x/=2
+# print(x)
+
+#----------------------------------------------------------------
+#AND ,OR, NOT
+
+# print((10==10)and (10>5)and (10>2))
+# print((10>=10)and (10<5)and (10>2))
+# print((10==10)or (10<4)or(10>4))
+# print((10==30)or (10<4)or(10<4))
+# print(not((10==30)or (10<4)or(10<4)))
+
+#---------------------------------------------------------------
+#is , is not
+#in not in
+# print("Dhara" is "Dhara ")
+# print("Dhara" is not "Dhara")
+# print("Dhara" in "Dharagai")
+# print("Dhara" not in "Dharagai")
+
